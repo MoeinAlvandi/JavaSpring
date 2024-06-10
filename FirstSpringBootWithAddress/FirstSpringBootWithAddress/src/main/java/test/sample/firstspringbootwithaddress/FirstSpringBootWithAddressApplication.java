9a6a2ed38be76f412375bb40a3a8c46ff3cf0c18
Moein Alvandi <mr.programmer1398@gmail.com>
2024-06-10 18:38:59 +0330

    FirstSpringBootWithAddress

---
+++ FirstSpringBootWithAddress/FirstSpringBootWithAddress/src/main/java/test/sample/firstspringbootwithaddress/FirstSpringBootWithAddressApplication.java
@@ -0,0 +1,13 @@
+package test.sample.firstspringbootwithaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootWithAddressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootWithAddressApplication.class, args);
+    }
+
+}
---
+++ FirstSpringBootWithAddress/FirstSpringBootWithAddress/src/test/java/test/sample/firstspringbootwithaddress/FirstSpringBootWithAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package test.sample.firstspringbootwithaddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootWithAddressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
